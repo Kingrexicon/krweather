d02d87021918c8dd6790e014dc1ba10bc92ce6ca
Oyedele oreoefeoluwa <oyedeleoreofe@gmail.com>
2022-10-06 14:56:48 +0100

    gotten details for the api

--- asyncc.js
+++ asyncc.js
@@ -1,42 +1,30 @@
-
-// let myName = "kunle"
-// const test = async() => {
-//     console.log(myName)
-//     delay()
-// }
-
-// const delay = () => {
-//     setTimeout(() => {
-//         myName = "sola"
-//     }, 3000
-//     )
-
-// }
-
-
-let myIndexle = dar.value
-let myKey = "fbfcdcdd51890c4dd9f73dbd8fe927d9"
-const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${myIndexle}&appid=${myKey}`
-const endpoint2 = "https://api.github.com/users"
-const tesIt = async()=>{
+let key = "fbfcdcdd51890c4dd9f73dbd8fe927d9"
+let locationInput = document.getElementById("location")
+// const endpoint2 = "https://api.github.com/users"
+async function tesIt() {
+    let userLocation = location.value;
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=ogbomoso&appid=${key}&units=metric`
+    // const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=${key}&units=metric`
     let response = await fetch(endpoint)
     let jsonResponse = await response.json()
-    // console.log(jsonResponse)
-    jsonResponse.map((item)=>{
-        disp.innerHTML +=`
-        <h1>${item.website}</h1>
-        <img src="${item.avatar_url}"/>
-        `
-        
-    })
-}
-// const getLocation = ()=>{
-//     navigator.geolocation.getCurrentPosition((position)=>{
-//         console.log(position.coords.latitude)
-//         console.log(position.coords.longitude)
-//     })
-// }
+    console.log(jsonResponse)
+    uName.innerHTML = ` ${jsonResponse.name}`;
+    uCountry.innerHTML = ` ${jsonResponse.sys.country}`;
+    uWeather.innerHTML = ` ${jsonResponse.weather[0].main}`;
+    uIcon.innerHTML= ` ${jsonResponse.weather[0].icon}}`;
+    uDescription.innerHTML=` ${jsonResponse.weather[0].description}`;
+    uTemp.innerHTML=` ${jsonResponse.main.temp}k`;
+    // uFeel.innerHTML =` ${jsonResponse.main.feels_like;
+    uLat.innerHTML =` ${jsonResponse.coord.lat}`;
+    uLon.innerHTML =` ${jsonResponse.coord.lon}`;
+    uWind.innerHTML =` ${jsonResponse.wind.deg}<sup>o</sup> at the speed of ${jsonResponse.wind.speed}m/s`;
+    uPressure.innerHTML= `${jsonResponse.main.pressure}hPa`;
+    uHumidity.innerHTML =`${jsonResponse.main.humidity}%`;
+    uClouds.innerHTML= `${jsonResponse.clouds.all}%`;
 
- fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
+
+
+}
+//  fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
--- chuck.js
+++ chuck.js
@@ -6,11 +6,13 @@ const test = {
 		'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
 	}
 };
-const testi = ()=>{
+const tesIt = ()=>{
 
-    fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', test)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .then(response => disp.innerHTML= response.categories['value'])
-        .catch(err => console.error(err));
+    fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random/', test)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        // .then(response => disp.innerHTML= response.categories)
+        .catch(err => console.log(err));
+		// .then((josn) )
+	jokes.innerHTML =JSON.stringify( categories.url)
 }
